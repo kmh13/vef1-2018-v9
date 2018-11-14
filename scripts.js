edd788217981a321a0f635b1078dfdf63e5663e6
kmh13 <42941511+kmh13@users.noreply.github.com>
2018-11-14 14:30:40 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ const API_URL = 'https://apis.is/isnic?domain=';
 const program = (() => {
   let domains;
 
-  function displayError(Villa) {
+  function displayError(Villa) { // displayar villu ef þess þarf
     const container = domains.querySelector('.results');
     while (container.firstChild) {
       container.removeChild(container.firstChild);
@@ -19,7 +19,7 @@ const program = (() => {
     container.appendChild(document.createTextNode(Villa));
   }
 
-  function El(name, data) {
+  function El(name, data) { // búum til element fyrir dl,dt,dd 
     if (data !== '') {
       const results = document.querySelector('.results');
       const dl = document.createElement('dl');
@@ -33,22 +33,22 @@ const program = (() => {
     }
   }
 
-  function dateToday(date) {
+  function dateToday(date) { // gefur núverandi dagsetningu 
     return new Date(date).toISOString().split('T')[0];
 }
-  function displayDomain(domainsList) {
+  function displayDomain(domainsList) {  //búum til fyrir niðurstöður
     const results = document.querySelector('.results');
     while (results.firstChild) {
       results.removeChild(results.firstChild);
     }
-    if (domainsList === undefined) {
+    if (domainsList === undefined) { // gefur út skilaboð ef len sem skrá er inn er ekki til 
       displayError('Len er ekki skráð');
       return;
     }
 
-    const dl = document.createElement('dl');
+    const dl = document.createElement('dl'); 
 
-    El('Lén', domainsList.domain);
+    El('Lén', domainsList.domain);  
     El('Skráð', dateToday(domainsList.registered));
     El('Seinast breytt', dateToday(domainsList.lastChange));
     El('Rennur út', dateToday(domainsList.expires));
@@ -56,20 +56,20 @@ const program = (() => {
     El('Netfang', domainsList.email);
     El('Heimilisfang', domainsList.address);
     El('Land', domainsList.country);
-    const container = domains.querySelector('.results');
+    const container = domains.querySelector('.results'); // setningar sem koma upp á skjá, lén, skráð og f.l
 
     container.appendChild(dl);
   }
 
 
-  function fetchData(info) {
+  function fetchData(info) { //nær í gögn 
     loading();
     fetch(`${API_URL}${info}`)
       .then((response) => {
         if (response.ok) {
           return response.json();
         }
-        throw new Error('Villa kom upp');
+        throw new Error('Villa kom upp'); // kallar inn skipun ef villa. 
       })
 
       .then((data) => {
@@ -85,36 +85,37 @@ const program = (() => {
     e.preventDefault();
     const input = e.target.querySelector('input');
     if (input.value.trim() === '') {
-      displayError('Lén verður að vera strengur');
+      displayError('Lén verður að vera strengur'); // skilaboð ef stimplað er inn lén sem er ekki strengur.
       document.querySelector('input').value = '';
       return;
     }
     fetchData(input.value);
   }
 
-  function init(_domains) {
+  function init(_domains) { // tengir við API urlinn 
     domains = _domains;
     const form = domains.querySelector('form');
     form.addEventListener('submit', onSubmit);
   }
 
 
-  function loading() {
+  function loading() { // bið takinn 
     const results = document.querySelector('.results');
     while (results.firstChild) {
       results.removeChild(results.firstChild);
     }
-    const div = document.createElement('div');
+    
+    const div = document.createElement('div'); // búum til div element sem við bættum inn í loading til þess að tengja 
     div.classList.add('loading');
 
-    const bida = document.createElement('img');
+    const bida = document.createElement('img'); // tengir gif við bið takann 
     bida.setAttribute('src', 'loading.gif');
     console.log(bida);
     results.appendChild(div);
     div.appendChild(bida);
 
 
-    const span = document.createElement('span');
+    const span = document.createElement('span'); // skilaboð sem kemur upp þegar verið er að leita af léni
     span.appendChild(document.createTextNode('Leita að léni...'));
     div.appendChild(span);
   }
@@ -124,7 +125,7 @@ const program = (() => {
   };
 })();
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { // eventlistener sem er hægt að nota með DOM
   const domains = document.querySelector('.domains');
   program.init(domains);
 });
